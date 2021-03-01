c69f861560ebfc1acefe3991887688e2f8b920f4
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-01 16:04:23 -0300

    Tipos de comentários usando /** enter our /*** enter para criar documentação oficial do Java

--- src/main/java/ImprimindoVariaveis.java
+++ src/main/java/ImprimindoVariaveis.java
@@ -1,6 +1,15 @@
 public class ImprimindoVariaveis {
 
-    public static void mains (String[] args){
+    // Voce pode comentar o que quiser
+    /*
+    Comentários são necessários
+     */
+
+    /***
+     * Esse e o metodo main
+     * @param args parametro de entrada do metodo Main
+     */
+    public static void main (String[] args){
         System.out.println("Dados impresos");
     }
 }
