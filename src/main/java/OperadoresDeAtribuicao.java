85098f421d094a020175a0275ad6b1f0b1d6f51c
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-03 14:31:20 -0300

    "Criado código de operadores de atribuição"

---
+++ src/main/java/OperadoresDeAtribuicao.java
@@ -0,0 +1,17 @@
+public class OperadoresDeAtribuicao {
+
+    public static void main(String[] args) {
+        // Operadores de atribuição: =, -=, +=, *=, /=, %=
+        //São feitos para poupar linha de código
+        int salario = 2000;
+        //Para acrescentar mas 1000 reais no salário, o código sería assim:
+        //salario = salario + 1000;
+        //salario += 1000;// Menos código escrito. Muito usado em laços de repetição
+        //salario *= 0.1;
+        salario = salario + (int) (salario * 0.1);
+        //Neste código foi colocado o int para forzar o
+        //programa a fazer a somatoria
+        //System.out.println(salario);
+        System.out.println(salario);
+    }
+}
