bd06b2fbb5a1641a63878974a55cfa8375940e03
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-03 09:42:01 -0300

    "Criado algoritmo pra salvar dados pessoais"

---
+++ src/main/java/ImprimiendoVariaveisDois.java
@@ -0,0 +1,20 @@
+public class ImprimiendoVariaveisDois {
+
+    /**
+     *
+     * Crie um algoritmo que possua variáveis pra salvar os seguintes dados:
+     * Nome, endereco e telefone de uma pessoa.
+     * Imprima esas variáveis da seguinte forma:
+     * 0 <nome> domiciliado no <endereco> e <telefone> não possui nenhum tipo de pendencia
+     */
+
+
+    public static void main (String [] args){
+        String name = "Simone Santos";
+        String address = "Rua Guilherme Santos número 93 ";
+        String telephone = "11 9999 6754";
+        System.out.println(name + " domiciliado no endereço: " + address + " e telefone: " + telephone + "," +
+                " não possui nenhuma pendencia " );
+
+    }
+}
