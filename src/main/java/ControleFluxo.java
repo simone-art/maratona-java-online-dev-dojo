1b1c5019effa8b5e61a61d50a31f6a84bb859f10
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-03 15:30:47 -0300

    "Criado código usando as estruturas condicionais If e else"

--- src/main/java/ControleFluxo.java
+++ src/main/java/ControleFluxo.java
@@ -7,12 +7,26 @@ public class ControleFluxo {
      */
     public static void main(String[] args) {
         // O if só entra na condição quando ela é verdadeira
-        int idade = 20;
-        float salario = 2000;
-        if (idade >= 18 && salario >= 2000) {
-            System.out.println("Você está dentro do clube dos DEV Jr.");
+//        int idade = 20;
+//        float salario = 2000;
+//        if (idade >= 18 && salario >= 2000) {
+//            System.out.println("Você está dentro do clube dos DEV Jr.");
+//        } else {
+//            System.out.println("Você não está dentro do clube dos DEV Jr!");
+//        }
+//    }
+
+        //idade < 15 categoria infantil
+        //idade >= 15 && idade < 18 Juvenil
+        // idade >= 18 Adulto
+        int idade = 15;
+        if (idade < 15) {
+            System.out.println("Você pertence a categoría infantil");
+        } else if (idade >= 15 && idade < 18) {
+            System.out.println("Você pertence a categoría Juvenil");
         } else {
-            System.out.println("Você não está dentro do clube dos DEV Jr!");
+            System.out.println("Você pertence a categoría adulto");
+
         }
     }
 }
