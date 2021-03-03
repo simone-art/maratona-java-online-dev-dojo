d667098e666a461967e3c651bc6317f1e507d807
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-03 13:51:56 -0300

    "Criado código de operadores lógicos"

---
+++ src/main/java/OperadoresLogicos.java
@@ -0,0 +1,11 @@
+public class OperadoresLogicos {
+    public static void main (String[] args){
+        int idade = 18;
+        float salario = 1000f;
+        System.out.println(idade >= 18 && salario >= 1000f);
+        System.out.println(idade >= 18 && salario >= 3000f);
+        System.out.println(idade >= 18 || salario >= 3000f);
+        //Com o ||, uma das condições tem que ser verdadeiras
+        //Com o &&, as duas condições tem que ser verdadeiras para se cumplir.
+    }
+}
