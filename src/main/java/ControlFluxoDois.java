25a028373b3e89d81d18167ea7e6431e4aa712ba
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-08 10:29:50 -0300

    "Criado classe para usar o if e else"

---
+++ src/main/java/ControlFluxoDois.java
@@ -0,0 +1,10 @@
+public class ControlFluxoDois {
+    public static void main(String[] args) {
+        int idade = 16;
+        if(idade >= 18){
+            System.out.println("Você é adulto");
+        }else{
+            System.out.println("Você não é adulto");
+        }
+    }
+}
