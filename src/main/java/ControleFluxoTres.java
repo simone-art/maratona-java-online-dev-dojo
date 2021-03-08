c0780941cce6069a82c24a5c120acca3aa908186
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-08 13:05:40 -0300

    "Criada classe para executar o switch"

---
+++ src/main/java/ControleFluxoTres.java
@@ -0,0 +1,32 @@
+public class ControleFluxoTres {
+    public static void main(String[] args) {
+        /*
+        Switch
+        switch() dentro desta estrutura vai a variável que podem ser char, int, byte, short, enum e String
+         */
+        byte dia = 1;
+        switch (dia){
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda Feira");
+                break;
+            case 3:
+                System.out.println("Terca feira");
+                break;
+            case 4:
+                System.out.println("Quarta Feira");
+                break;
+            case 5:
+                System.out.println("Quinta Feira");
+                break;
+            case 6:
+                System.out.println("Sexta Feira");
+                break;
+            case 7:
+                System.out.println("Sábado");
+                break;
+        }
+    }
+}
