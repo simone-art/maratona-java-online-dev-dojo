2f9ce2dd3bd0cdb77534d1426312b76caa2229f3
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-08 13:33:14 -0300

    "Criado programa usando switch pra imprimar se é día útil ou final de semana"

---
+++ src/main/java/ExercicioControleFluxoDois.java
@@ -0,0 +1,36 @@
+public class ExercicioControleFluxoDois {
+    public static void main(String[] args) {
+        /*
+        Cria um switch que dado um valor de 1 até 7,
+        considerando o valor 1 igual a domingo,
+        imprima se é día útil ou final de semana
+         */
+
+        byte diaUtil = 1;
+        switch (diaUtil) {
+            case 1:
+                System.out.println("Hoje é domingo, final de semana");
+                break;
+            case 2:
+                System.out.println("Hoje é segunda, día útil");
+                break;
+            case 3:
+                System.out.println("Hoje é terça, día útil");
+                break;
+            case 4:
+                System.out.println("Hoje é quarta, día útil");
+                break;
+            case 5:
+                System.out.println("Hoje é quinta, día útil");
+                break;
+            case 6:
+                System.out.println("Hoje é sexta, día útil");
+                break;
+            case 7:
+                System.out.println("Hoje é sábado, final de semana");
+                break;
+            default:
+                System.out.println("Opção inválida");
+        }
+    }
+}
