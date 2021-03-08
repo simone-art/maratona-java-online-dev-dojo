dbddd9a2fe91d5ec1653b39bd4c2eac07d1dfe82
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-08 10:53:57 -0300

    "Uso de operador ternário PRA inicializar uma variável e ter uma instrução de uma linha só"

--- src/main/java/ControlFluxoDois.java
+++ src/main/java/ControlFluxoDois.java
@@ -2,11 +2,14 @@ public class ControlFluxoDois {
     public static void main(String[] args) {
         int idade = 25;
         String status;
-        if(idade >= 18){
-            status = "Você é adulto";
-        }else{
-            status = "Você não é adulto";
-        }
+//        if(idade >= 18){
+//            status = "Você é adulto";
+//        }else{
+//            //Uso de operador ternário e úma forma de inicializar uma variável
+//            //utilizando uma instrução de uma linha só
+//            status = "Você não é adulto";
+//        }
+        status = idade > 18? "Você é adulto" : "adulto";
         System.out.println(status);
     }
 }
