baa9f198798d6bf85e33f27c265b12de356ed327
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-15 14:13:11 -0300

    "Criado código que calcula número impar usando o for e sem definir variável"

--- src/main/java/ExercicioControleFluxoTres.java
+++ src/main/java/ExercicioControleFluxoTres.java
@@ -1,12 +1,11 @@
 public class ExercicioControleFluxoTres {
     public static void main(String[] args) {
         //Imprima todos os números pares de 0 até 100000
-        //1.Definir uma variável
+        //1.Sem definir uma variável
         //2.Definir um for
 
-        int listaNumero = 100000;
-        for (int i = 0; i <= listaNumero; i++) {
-            if (i % 2 != 0) {
+        for (int i = 0; i <= 100000; i++) {
+            if (i % 2 == 0) {
                 System.out.println("Este valor é impar " + i);
             }
         }
