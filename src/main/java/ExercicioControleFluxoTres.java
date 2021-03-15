2ac9dcbfc70b7bf518ed3c819451ffe1a8152e25
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-15 14:10:03 -0300

    "Criado código que calcula número par"

---
+++ src/main/java/ExercicioControleFluxoTres.java
@@ -0,0 +1,15 @@
+public class ExercicioControleFluxoTres {
+    public static void main(String[] args) {
+        //Imprima todos os números pares de 0 até 100000
+        //1.Definir uma variável
+        //2.Definir um for
+
+        int listaNumero = 100000;
+        for (int i = 0; i <= listaNumero; i++) {
+            if (i % 2 == 0) {
+                System.out.println("Este valor é par " + i);
+            }
+        }
+    }
+}
+
