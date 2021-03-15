f2e859ae20410b055106d322075379e4da9e13d6
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-15 11:01:49 -0300

    "Incluida variável porcentagem como string"

--- src/main/java/ExercicioControleFluxoCinco.java
+++ src/main/java/ExercicioControleFluxoCinco.java
@@ -3,15 +3,19 @@ public class ExercicioControleFluxoCinco {
     //imprima 30% do valor. Senão imprima 15
 
     public static void main(String[] args) {
-        double salario = 4500;
+        double salario = 3500;
+        double resultado;
+        String porcentagem = "";
 
         if (salario > 4500){
-            double resultado = salario * 30 / 100;
-            System.out.println("O valor do imposto do seu salário é: " + resultado);
+            resultado = salario * 30 / 100;
+            porcentagem = "30%";
         }else {
-            double resultado = salario * 15 / 100;
-            System.out.println("O valor do imposto do seu salário é: " + resultado);
+            resultado = salario * 15 / 100;
+            porcentagem = "10%";
+
         }
+        System.out.println("O valor do imposto de " + porcentagem + " do seu salário é: " + resultado);
     }
 
 }
