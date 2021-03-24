a85fb90eb5922918798f78cafaec0a51196a697f
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-24 13:35:26 -0300

    "Criado classe Array em Java"

---
+++ src/main/java/Array1.java
@@ -0,0 +1,17 @@
+public class Array1 {
+    public static void main(String[] args) {
+
+        //Sintaxi do array
+        //Pra inicializar um array em java, deve-se usar a palavra new
+        //Voce não pode criar um array sem tamanho em java
+        //A palavra new se usa para criar objetos
+        int [] idades = new int[3];
+        idades [0] = 20;
+        idades [1] = 15;
+        idades [2] = 18;
+        System.out.println("Idade 1: " + idades[0]);
+        System.out.println("Idade 2: " + idades[1]);
+        System.out.println("Idade 3: " + idades[2]);
+
+    }
+}
