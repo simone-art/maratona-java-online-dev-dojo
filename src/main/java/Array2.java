19a1239a0624e61e8d123c9a00c41d882e61e26b
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-24 13:46:41 -0300

    "Criada classe array2 que ao definir variável ainda sem colocar os valores, por padrão ela vem com o zero"

---
+++ src/main/java/Array2.java
@@ -0,0 +1,9 @@
+public class Array2 {
+    public static void main(String[] args) {
+        int [] idades = new int[3];
+
+        System.out.println("Idade 1: " + idades[0]);
+        System.out.println("Idade 2: " + idades[1]);
+        System.out.println("Idade 3: " + idades[2]);
+    }
+}
