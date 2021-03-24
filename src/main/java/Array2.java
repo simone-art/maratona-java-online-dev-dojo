200f347738b3a0b5c454e8606e6448fa5d083597
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-24 16:59:08 -0300

    "Usando o for dentro do array pra "

--- src/main/java/Array2.java
+++ src/main/java/Array2.java
@@ -9,10 +9,9 @@ public class Array2 {
         nomes [0] = "Maria";
         nomes [1] = "Rogerio";
         nomes [2] = "Simone";
-        //for(int i = 0; i < 3; i++)
+        for(int i = 0; i < 3; i++)
 
-        System.out.println("Nome 1: " + nomes[0]);
-        System.out.println("Nome 2: " + nomes[1]);
-        System.out.println("Nome 3: " + nomes[2]);
+        System.out.println("Nomes: " + nomes[i]);
+
     }
 }
