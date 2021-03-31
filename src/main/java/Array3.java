db366d3895d8b654759028bd1728b9cb86d5a629
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-31 11:32:19 -0300

    "Formas diferentes para criar um array em Java"

---
+++ src/main/java/Array3.java
@@ -0,0 +1,12 @@
+public class Array3 {
+    public static void main(String[] args) {
+        //3 formas para criar um array em Java
+        int [] numeros = new int[5];
+        int [] numeros2 = {1,2,3,4,5}; //Tamanho = 5 mas o index vai até 4 [0, 4]
+        int [] numeros3 = new int[]{1,2,3,4,5};
+
+        for(int i = 0; i<numeros2.length; i++){
+            System.out.println(numeros2[i]);
+        }
+    }
+}
