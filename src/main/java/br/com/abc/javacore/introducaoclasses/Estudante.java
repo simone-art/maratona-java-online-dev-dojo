ef76337bd79a8333ba50b5f678210dd741c15a98
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-31 17:44:01 -0300

    "Criadas classe Estudante para aprender sobre programação orientada a objeto"

---
+++ src/main/java/br/com/abc/javacore/introducaoclasses/Estudante.java
@@ -0,0 +1,9 @@
+package br.com.abc.javacore.introducaoclasses;
+
+import java.net.PortUnreachableException;
+
+public class Estudante {
+    public String nome;
+    public String matricula;
+    public int idade;
+}
