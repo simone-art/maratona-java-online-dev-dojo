8341edf09b1c058fd08ec7d296303446ad2dfd29
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-31 13:24:44 -0300

    "Criada classe Array4 para iterar array de nomes"

---
+++ src/main/java/Array4.java
@@ -0,0 +1,9 @@
+public class Array4 {
+    public static void main(String[] args) {
+        String[] nomes = {"Vinicius", "João", "Juliette"};
+        for(String nome : nomes){
+            System.out.println(nome);
+        }
+
+    }
+}
