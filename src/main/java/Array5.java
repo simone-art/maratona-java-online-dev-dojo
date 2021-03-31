7613c6e1451c0e07a2f89c5b48fbb0fcda1d6e20
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-31 15:56:39 -0300

    "Criada classe Array5 para fazer arrays multidimensionais"

---
+++ src/main/java/Array5.java
@@ -0,0 +1,21 @@
+public class Array5 {
+    public static void main(String[] args) {
+
+        //Arrays multidimensionais se criam com um corchete a mais
+        //dias[0][0] = 30; o primer zero é do array, o outro é da posição
+        //int[0][1] dias = new int[0][0][0][1] [1][0][1][1]
+        int [][] dias = new int[2][2];
+        dias[0][0] = 30;
+        dias[0][1] = 29;
+        dias[1][0] = 28;
+        dias[1][1] = 27;
+
+        for(int i = 0; i< dias.length; i++){
+            //System.out.println(dias[i]);
+            for(int j = 0; j <dias[i].length; j++){
+                System.out.println(dias[i][j]);
+            }
+        }
+        
+    }
+}
