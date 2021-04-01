7a98ce3f724179285db2074c563bb78fdbb81569
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-04-01 12:46:00 -0300

    "Criados métodos básicos que somam e subtraem valores"

---
+++ src/main/java/br/com/abc/javacore/introducaometodos/classes/Calculadora.java
@@ -0,0 +1,17 @@
+package br.com.abc.javacore.introducaometodos.classes;
+
+public class Calculadora {
+    public void somaDoisNumeros(){
+        int numA = 5;
+        int numB = 3;
+        int resultado = numA + numB;
+        System.out.println("A soma é:" + resultado);
+    }
+
+    public void substracaoDoisNumeros(){
+        int numA = 5;
+        int numB = 3;
+        int resultado = numA - numB;
+        System.out.println("A substração é: " + resultado);
+    }
+}
---
+++ src/main/java/br/com/abc/javacore/introducaometodos/test/CalculadoraTest.java
@@ -0,0 +1,11 @@
+package br.com.abc.javacore.introducaometodos.test;
+
+import br.com.abc.javacore.introducaometodos.classes.Calculadora;
+
+public class CalculadoraTest {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.somaDoisNumeros();
+        calculadora.substracaoDoisNumeros();
+    }
+}
